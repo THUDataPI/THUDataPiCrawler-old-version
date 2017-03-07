51770f60345d4047552eaf2ac7d5a28412a92ec0
bearnxx <nxg2006@163.com>
2017-03-07 21:57:06 +0800

    Initial DB with this file

---
+++ InitDB.sql
@@ -0,0 +1,12 @@
+DROP TABLE IF EXISTS `lagou`.`company`;
+CREATE TABLE  `lagou`.`company` (
+  `companyId` varchar(20) NOT NULL,
+  `companyShortName` varchar(100) DEFAULT NULL,
+  `companyFullName` varchar(200) DEFAULT NULL,
+  `financeStage` varchar(45) DEFAULT NULL,
+  `city` varchar(30) DEFAULT NULL,
+  `industryField` varchar(45) DEFAULT NULL,
+  `companySize` varchar(20) DEFAULT NULL,
+  `Dealed` varchar(5) DEFAULT '0',
+  PRIMARY KEY (`companyId`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='公司主表';
