2f885e7c162a8f48027ea0fce539a5cca73d5490
Ziliang Wang <wangziliang05@gmail.com>
2017-03-28 19:04:00 +0800

    Add files via upload

--- THUDataPiCrawler_chinahr/THUDataPiCrawler_chinahr/THUDataPiCrawler/settings.py
+++ THUDataPiCrawler_chinahr/THUDataPiCrawler_chinahr/THUDataPiCrawler/settings.py
@@ -57,12 +57,12 @@ USER_AGENTS = [
 ROBOTSTXT_OBEY = False
 
 # Configure maximum concurrent requests performed by Scrapy (default: 16)
-CONCURRENT_REQUESTS = 2
+CONCURRENT_REQUESTS = 8
 
 # Configure a delay for requests for the same website (default: 0)
 # See http://scrapy.readthedocs.org/en/latest/topics/settings.html#download-delay
 # See also autothrottle settings and docs
-DOWNLOAD_DELAY = 30
+DOWNLOAD_DELAY = 5
 # The download delay setting will honor only one of:
 #CONCURRENT_REQUESTS_PER_DOMAIN = 16
 #CONCURRENT_REQUESTS_PER_IP = 16
